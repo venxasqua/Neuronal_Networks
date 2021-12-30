b01711fe0a7caddf6fd73f8270e53b40b8cfd278
venxasqua <venxasqua@gmail.com>
2021-12-30 20:40:57 +0100

    mode added

--- MLPClassifier.py
+++ MLPClassifier.py
@@ -1,8 +1,9 @@
 import numpy as np
 import pandas as pd
+import numba as nb
 class MLPClassifier:
     def __init__(self,hidden_layer_size,alpha=0.0001,learning_rate=0.1,
-                max_iter=2000, random_state=42,momentum = 0.9,use_momentum = True):
+                max_iter=2000, random_state=42,momentum = 0.9,use_momentum = True,mode="multi-task",threshold=0.5):
 
         self.hidden_layer_size : tuple = hidden_layer_size
         self.alpha : float = alpha
@@ -10,8 +11,40 @@ class MLPClassifier:
         self.max_iter : int = max_iter
         self.random_state = random_state
         self.momentum = momentum
+        """mode by default : multi-task but you can also choose softmax"""
+        self.mode = mode
         if not use_momentum:
              self.momentum = 0
+        self.threshold = threshold
+    def __install_layer_array(self,X_shape,Y_shape):
+        np.random.seed(self.random_state)
+        self.W = list(range(len(self.hidden_layer_size)+1))
+
+        self.b = list(range(len(self.hidden_layer_size)+1))
+
+        self.W[0] = np.random.randn(
+                                    self.hidden_layer_size[0],X_shape[0]
+                                    )
+
+        self.b[0] = np.zeros((self.hidden_layer_size[0],1))
+
+        self.W[len(self.hidden_layer_size)] = np.random.randn(
+                                                        Y_shape[0],
+                                                        self.hidden_layer_size[
+                                                            len(self.hidden_layer_size)-1
+                                                             ])
+
+        self.b[len(self.hidden_layer_size)] = np.zeros((Y_shape[0],1))
+
+        for i in range(1,len(self.hidden_layer_size)):
+
+            self.W[i] = np.random.randn(self.hidden_layer_size[i],
+                                        self.hidden_layer_size[i-1]
+                                        )
+
+            self.b[i] = np.zeros((self.hidden_layer_size[i],1))
+
+
 
     def fit(self,X_train : np.array, Y_train : np.array):
         """ X_train is an array of dimension (m,n),
@@ -19,7 +52,7 @@ class MLPClassifier:
             n is the numbers of feature
             Y_train is an array of dimension (m,1) with value of type int"""
         """ some preparation"""
-        np.random.seed(self.random_state)
+ 
         X_train = X_train.T  
  
         """ we get the dummies matrix for Y_train so, if we want to identify
@@ -30,11 +63,10 @@ class MLPClassifier:
         Y_train = pd.get_dummies(
                                 Y_train.reshape(Y_train.shape[0])
                                 ).values.T
-        
-        self.W = list(range(len(self.hidden_layer_size)+1))
 
-        self.b = list(range(len(self.hidden_layer_size)+1))
+        self.__install_layer_array(X_train.shape,Y_train.shape)
 
+        # set the gradient at 0
         dZ = list(range(len(self.hidden_layer_size)+1))
 
         dW = [0]*(len(self.hidden_layer_size)+1)#list(range(len(self.hidden_layer_size)+1))
@@ -42,28 +74,6 @@ class MLPClassifier:
         db = [0]*(len(self.hidden_layer_size)+1)#list(range(len(self.hidden_layer_size)+1))
 
         sigmoid = list(range(len(self.hidden_layer_size)+1))
-
-        self.W[0] = np.random.randn(
-                                    self.hidden_layer_size[0],X_train.shape[0]
-                                    )
-
-        self.b[0] = np.zeros((self.hidden_layer_size[0],1))
-
-        self.W[len(self.hidden_layer_size)] = np.random.randn(
-                                                        Y_train.shape[0],
-                                                        self.hidden_layer_size[
-                                                            len(self.hidden_layer_size)-1
-                                                             ])
-
-        self.b[len(self.hidden_layer_size)] = np.zeros((Y_train.shape[0],1))
-
-        for i in range(1,len(self.hidden_layer_size)):
-
-            self.W[i] = np.random.randn(self.hidden_layer_size[i],
-                                        self.hidden_layer_size[i-1]
-                                        )
-
-            self.b[i] = np.zeros((self.hidden_layer_size[i],1))
 
         #------------------------------------------------------------------------------------------------------------------------------
         """now we compute the activation function of each neuron on every layer"""
@@ -75,17 +85,25 @@ class MLPClassifier:
 
             sigmoid[0] = 1/(1 + np.exp(-(self.W[0].dot(X_train) + self.b[0])))
 
-            for j in range(1,len(self.hidden_layer_size)):
+            for j in range(1,len(self.hidden_layer_size)+1):
 
                 sigmoid[j] = 1/(1 + np.exp(-(self.W[j].dot(sigmoid[j-1]) + self.b[j])))
-            z = self.W[len(self.hidden_layer_size)].dot(sigmoid[len(self.hidden_layer_size)-1]
-                                                        ) + self.b[len(self.hidden_layer_size)]
-            z = np.exp(z)
-            sigmoid[len(self.hidden_layer_size)] = z/z.sum(axis=0)
+            """for the last layer we use mode to compute the activasion function"""
+            if self.mode == "softmax":
+                z = self.W[len(self.hidden_layer_size)].dot(sigmoid[len(self.hidden_layer_size)-1]
+                                                            ) + self.b[len(self.hidden_layer_size)]
+                z = np.exp(z)
+                sigmoid[len(self.hidden_layer_size)] = z/z.sum(axis=0)
+
+
             #computer the cost function
             """I put the cost function here, but you can also put at the end of  this methode.
                 The purpose was, that maybe some one want to plot the cost function after each iteration"""
-            cost_function = -(Y_train*np.log(sigmoid[len(self.hidden_layer_size)])).sum()/Y_train.shape[1]
+            cost_function = -(Y_train*np.log(sigmoid[len(self.hidden_layer_size)]
+                                            )+(1-Y_train)*np.log(1-sigmoid[len(self.hidden_layer_size)])
+                            ).sum()/Y_train.shape[1]
+            if self.mode == "softmax":
+                cost_function = -(Y_train*np.log(sigmoid[len(self.hidden_layer_size)])).sum()/Y_train.shape[1]
 
             """backward propagation"""
             dZ[len(self.hidden_layer_size)] = sigmoid[len(self.hidden_layer_size)] - Y_train
@@ -129,30 +147,47 @@ class MLPClassifier:
                 self.b[l] = (1-self.learning_rate*self.alpha/X_train.shape[1]
                             )*self.b[l] - self.learning_rate*db[l]
 
-        
-
         return cost_function
 
 
 
-    def predict(self,X_test : np.array,threshold=0.6):
-        
+    def predict(self,X_test : np.array):
         prediction_array = X_test.T
-
         for i in range(len(self.hidden_layer_size)+1):
 
             prediction_array = self.W[i].dot(prediction_array) + self.b[i]
 
             prediction_array = 1/(1+np.exp(-prediction_array))
-        return prediction_array.T.argmax(axis=1)
+        if self.mode == "softmax":
+            return prediction_array.T.argmax(axis=1)
+        prediction_array = prediction_array > self.threshold
+        return self.predict_help(prediction_array)
+
+
+    @staticmethod
+    @nb.jit(nopython=True)
+    def predict_help(prediction_array):
+        """ I want just to test numba and I dont know how to return
+            all max index along an axis for prediction_array so I use a loop"""
+        predict = []
+        for i in range(prediction_array.shape[1]):
+            b = []
+            for j in range(prediction_array.shape[0]):
+                if prediction_array[j,i]:
+                    b.append(j)
+                if j == prediction_array.shape[0]-1 and not b:
+                    b.append(-1)
+            predict.append(b)
+        return predict
+
 
 if __name__ == "__main__":
-    X_train =np.array([[-100,-10],[0,2],[100,150],[-30,-1],[1,-2],[200,105],[4,5],[2,3]])
-    Y_train = np.array([0,1,2,0,1,2,1,1])
+    X_train =np.array([[-100,-10],[0,2],[140,150],[-30,-1],[1,-2],[200,105],[4,5],[2,3],[101,105],[102,100],[103,130],[104,150],[142,125],[160,180]])
+    Y_train = np.array([0,1,2,0,1,2,1,1,2,2,2,2,2,2])
     Y_train=Y_train.reshape((Y_train.shape[0],1))
 
-    model = MLPClassifier((2,3,2),alpha=0.000001,max_iter=10000)
+    model = MLPClassifier((2,3,2),alpha=0.001,max_iter=2000,mode="0",threshold=0.6)
     print(model.fit(X_train,Y_train))
-    X_test = np.array([[-40,-10],[3,2],[100,150],[-5,-2],[2,-2],[15,25]])
+    X_test = np.array([[-40,-10],[3,2],[180,150],[-5,-2],[2,-2],[15,25]])
     d=model.predict(X_test)
     print(d)
--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # Neuronal_Networks
 This is my first learning project to Neuronal Network.
 
+requirements:
+numba, numpy and pandas
+
+
 There is some thing Iwant to say here.
 
 First, my Neuronal network can be used for Multi-class Classification.
 Here, I used one vs all. So if you want to recognize c number of classes, then Y_train is a (m,1) dimensional matrix, where m is the training size, and Y_train has values between 0 and c-1. 
-Note here that the last layer (out-put layer) will have only c-1 neurons, so that if the acitivision function of the last layer is to low, then this belong to the first class.
+By default you use mode == "multi-task", so if you want to for exaple to detect multiple things at once and if the classes you want to dectect is not there then it returns -1, you also should to change threshold.
+The second mode you can use is "softmax", this will returns the class with the highest possibility.
 
 
